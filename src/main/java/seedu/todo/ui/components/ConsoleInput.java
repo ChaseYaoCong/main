17a0b9501f21719eec2091acd6b36c3461ddab9e
Chase <tiongyaocong@gmail.com>
2016-10-10 22:45:43 +0800

    Create commandResultStub and LogicStub

--- src/main/java/seedu/todo/ui/components/ConsoleInput.java
+++ src/main/java/seedu/todo/ui/components/ConsoleInput.java
@@ -2,6 +2,8 @@ package seedu.todo.ui.components;
 
 import java.util.logging.Logger;
 
+import com.google.common.eventbus.Subscribe;
+
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.SplitPane;
@@ -12,7 +14,9 @@ import javafx.stage.Stage;
 import seedu.todo.logic.Logic;
 import seedu.todo.ui.components.Console;
 import seedu.todo.ui.UiPartLoader;
-//import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.CommandResult;
+//import seedu.todo.commons.events.ui.IncorrectCommandAttemptedEvent;
+//import seedu.todo.logic.commands.CommandResult;
 import seedu.todo.commons.core.LogsCenter;
 import seedu.todo.ui.components.ConsoleInput;
 import seedu.todo.commons.util.FxViewUtil;
@@ -25,20 +29,23 @@ public class ConsoleInput extends Component {
     private AnchorPane commandPane;
     private Console consoleDisplay;
 
-    private Logic logic;
+    private LogicStub logic;
 	
 	// Props
 	public String lastCommandEntered;
+	@FXML
+	private TextField commandTextField;
+	private CommandResultStub mostRecentResult;
 	
    public static ConsoleInput load(Stage primaryStage, AnchorPane commandBoxPlaceholder,
-            Console resultDisplay, Logic logic) {
+            Console resultDisplay, LogicStub logic) {
         ConsoleInput commandBox = UiPartLoader.loadUiPart(primaryStage, commandBoxPlaceholder, new ConsoleInput());
         commandBox.configure(resultDisplay, logic);
         commandBox.addToPlaceholder();
         return commandBox;
     }
     
-    public void configure(Console resultDisplay, Logic logic) {
+    public void configure(Console resultDisplay, LogicStub logic) {
         this.consoleDisplay = resultDisplay;
         this.logic = logic;
         registerAsAnEventHandler(this);
@@ -50,11 +57,6 @@ public class ConsoleInput extends Component {
         FxViewUtil.applyAnchorBoundaryParameters(commandPane, 0.0, 0.0, 0.0, 0.0);
         FxViewUtil.applyAnchorBoundaryParameters(commandTextField, 0.0, 0.0, 0.0, 0.0);
     }
-	
-	@FXML
-    private TextField commandTextField;
-	//Command result by logic commands uncomment the line below when backend is done
-    //private CommandResult mostRecentResult;
 	
 	@Override
 	public String getFxmlPath() {
@@ -80,8 +82,62 @@ public class ConsoleInput extends Component {
 	/** ================ ACTION HANDLERS ================== **/
 	@FXML
 	public void handleConsoleInputChanged() {
-		// TODO
+        //Take a copy of the command text
+	    lastCommandEntered = commandTextField.getText();
+
+        /* We assume the command is correct. If it is incorrect, the command box will be changed accordingly
+         * in the event handling code {@link #handleIncorrectCommandAttempted}
+         */
+        setStyleToIndicateCorrectCommand();
+        mostRecentResult = logic.execute(lastCommandEntered);
+        consoleDisplay.postMessage(mostRecentResult.feedbackToUser);
+        logger.info("Result: " + mostRecentResult.feedbackToUser);
 	}
 	
+	/**
+     * Sets the command box style to indicate a correct command.
+     */
+    private void setStyleToIndicateCorrectCommand() {
+        commandTextField.getStyleClass().remove("error");
+        commandTextField.setText("");
+    }
 
+    @Subscribe
+    private void handleIncorrectCommandAttempted(IncorrectCommandAttemptedEvent event){
+        logger.info(LogsCenter.getEventHandlingLogMessage(event,"Invalid command: " + lastCommandEntered));
+        setStyleToIndicateIncorrectCommand();
+        restoreCommandText();
+    }
+
+    /**
+     * Restores the command box text to the previously entered command
+     */
+    private void restoreCommandText() {
+        commandTextField.setText(lastCommandEntered);
+    }
+
+    /**
+     * Sets the command box style to indicate an error
+     */
+    private void setStyleToIndicateIncorrectCommand() {
+        commandTextField.getStyleClass().add("error");
+    }
+
+    //CommandResultStub
+    class CommandResultStub {
+        public final String feedbackToUser;
+
+        public CommandResultStub(String feedbackToUser) {
+            assert feedbackToUser != null;
+            this.feedbackToUser = feedbackToUser;
+        }
+    } 
+    
+    class LogicStub {
+        CommandResultStub execute(String commandText) {
+            return new CommandResultStub(commandText);
+        }
+    }
 }
+
+
