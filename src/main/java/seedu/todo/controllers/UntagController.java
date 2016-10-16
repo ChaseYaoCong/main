d70872257ee6f4592034c73af74365a4ce2fe6ad
Chase <tiongyaocong@gmail.com>
2016-10-17 00:59:13 +0800

    create untag controller

---
+++ src/main/java/seedu/todo/controllers/UntagController.java
@@ -0,0 +1,68 @@
+package seedu.todo.controllers;
+
+import seedu.todo.commons.EphemeralDB;
+import seedu.todo.models.CalendarItem;
+import seedu.todo.models.Event;
+import seedu.todo.models.Task;
+import seedu.todo.models.TodoListDB;
+import seedu.todo.ui.UiManager;
+import seedu.todo.ui.views.IndexView;
+
+public class DestroyController implements Controller {
+    
+    private static String NAME = "Untag";
+    private static String DESCRIPTION = "Untag a task/event by listed index";
+    private static String COMMAND_SYNTAX = "untag <index>";
+    
+    private static String MESSAGE_DELETE_SUCCESS = "Tag has been removed!\n" + "To undo, type \"undo\".";
+    private static String MESSAGE_INVALID_CALENDARITEM = "Could not untag task/event: invalid index provided!";
+    
+    private static CommandDefinition commandDefinition =
+            new CommandDefinition(NAME, DESCRIPTION, COMMAND_SYNTAX); 
+
+    public static CommandDefinition getCommandDefinition() {
+        return commandDefinition;
+    }
+
+    @Override
+    public float inputConfidence(String input) {
+        // TODO
+        return (input.startsWith("untag") ? 1 : 0;
+    }
+
+    @Override
+    public void process(String args) {
+        // TODO: Example of last minute work
+        
+        // Get index.
+        int index = Integer.decode(args.replaceFirst("untag", "").trim());
+        
+        // Get record
+        EphemeralDB edb = EphemeralDB.getInstance();
+        CalendarItem calendarItem = edb.getCalendarItemsByDisplayedId(index);
+        TodoListDB db = TodoListDB.getInstance();
+        
+        if (calendarItem == null) {
+            UiManager.updateConsoleMessage(MESSAGE_INVALID_CALENDARITEM);
+            return;
+        }
+        
+        assert calendarItem != null;
+        
+//        if (calendarItem instanceof Task) {
+//            
+//        } else {
+//            db.destroyEvent((Event) calendarItem);
+//        }
+        
+        // Re-render
+        IndexView view = UiManager.loadView(IndexView.class);
+        view.tasks = db.getAllTasks();
+        view.events = db.getAllEvents();
+        UiManager.renderView(view);
+        
+        // Show console message
+        UiManager.updateConsoleMessage(MESSAGE_DELETE_SUCCESS);
+    }
+
+}
