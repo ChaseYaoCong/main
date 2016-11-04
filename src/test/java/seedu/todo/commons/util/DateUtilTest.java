9d8dfe9bc3cb76d483e9591f0ac21df3b9b41818
Chase <tiongyaocong@gmail.com>
2016-11-04 08:34:07 +0800

    update DateUtil test

--- src/test/java/seedu/todo/commons/util/DateUtilTest.java
+++ src/test/java/seedu/todo/commons/util/DateUtilTest.java
@@ -14,15 +14,24 @@ import org.junit.Test;
 import seedu.todo.commons.util.DateUtil;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
+ * @@author A0139812A
  * Test cases for {@link DateUtil}.
  * 
- * @@author A0139812A
  */
 public class DateUtilTest {
-	
+    LocalDateTime currentDay = LocalDateTime.now();
+    LocalDateTime currentDayAt2Pm = currentDay.toLocalDate().atTime(2, 0);
+    LocalDateTime nextDay = currentDay.plusDays(1);
+    LocalDateTime nextDayAt2Pm = nextDay.toLocalDate().atTime(2, 0);
+    LocalDateTime nextDayAt2359 = nextDay.toLocalDate().atTime(23, 59);
+    
 	@Test
 	public void toDate_equalTimestamps() {
 		long currTimeMs = java.lang.System.currentTimeMillis();
@@ -68,6 +77,32 @@ public class DateUtilTest {
 		assertEquals(DateUtil.floorDate(null), null);
 	}
 	
+	//@@author A0139922Y
+    @Test
+    public void ceilDate_sameDate() {
+        LocalDateTime today = LocalDateTime.now().toLocalDate().atTime(10,0);
+        LocalDateTime todayPlusAnHour = today.plusHours(1);
+        assertNotNull(DateUtil.ceilDate(today));
+        assertNotNull(DateUtil.ceilDate(todayPlusAnHour));
+        assertEquals(DateUtil.ceilDate(today), DateUtil.ceilDate(todayPlusAnHour));
+    }
+    
+    //@@author A0139922Y
+    @Test
+    public void ceilDate_differentDate() {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime tmr = LocalDateTime.now().plusDays(1);
+        assertNotNull(DateUtil.ceilDate(today));
+        assertNotNull(DateUtil.ceilDate(tmr));
+        assertNotEquals(DateUtil.ceilDate(today), DateUtil.ceilDate(tmr));
+    }
+    
+    //@@author A0139922Y
+    public void ceilDate_null_test() {
+        LocalDateTime nullDate = null;
+        assertEquals(null, DateUtil.ceilDate(nullDate));
+    }
+	
 	@Test
 	public void formatDayTests() {
 	    LocalDateTime now = LocalDateTime.now();
@@ -105,6 +140,13 @@ public class DateUtilTest {
 	    assertEquals(DateUtil.formatShortDate(pastDate), pastDate.format(DateTimeFormatter.ofPattern("E dd MMM")));
 	}
 	
+	//@@author A0139922Y
+    @Test
+    public void formatShortDateTests_null_test() {
+        assertNull(DateUtil.formatShortDate(null));
+        assertNotNull(DateUtil.formatShortDate(LocalDateTime.now()));
+    }
+	
 	@Test
 	public void parseShortDateTests() {
 	    // TODO
@@ -119,8 +161,131 @@ public class DateUtilTest {
 	    assertEquals(DateUtil.formatTime(LocalDateTime.of(2016, 1, 1, 23, 59)), "23:59");
 	}
 	
+	//@@author A0139922Y
+	@Test
+	public void formatTime_null_test() {
+	    assertNull(DateUtil.formatTime(null));
+	    assertNotNull(DateUtil.formatTime(LocalDateTime.now()));
+	}
+	
 	private static LocalDateTime fromEpoch(long epoch) {
 		return LocalDateTime.ofInstant(Instant.ofEpochMilli(epoch), ZoneId.systemDefault());
 	}
 	
+    //@@author A0139922Y	
+    @Test
+    public void testParseNatural() {
+        assertNull(DateUtil.parseNatural("todar"));
+        assertNotNull(DateUtil.parseNatural("today"));
+        assertEquals(DateUtil.floorDate(LocalDateTime.now()), DateUtil.floorDate(DateUtil.parseNatural("today")));
+    }
+    
+    //@@author A0139922Y	
+    @Test
+    public void testIfDateExist_false() {
+        LocalDateTime currentDate = LocalDateTime.now();
+        assertNotNull(DateUtil.checkIfDateExist(currentDate));
+        assertFalse(DateUtil.checkIfDateExist(currentDate));
+    }
+    
+    //@@author A0139922Y
+    public void testCheckIfDateExist_true() {
+        LocalDateTime currentDate = LocalDateTime.now();
+        assertNotNull(DateUtil.checkIfDateExist(currentDate));
+        //only year is diff
+        assertTrue(DateUtil.checkIfDateExist(currentDate.plusYears(1)));
+        //only month is diff
+        assertTrue(DateUtil.checkIfDateExist(currentDate.plusMonths(1)));
+        assertTrue(DateUtil.checkIfDateExist(currentDate.plusMonths(12)));
+        //only day is diff
+        assertTrue(DateUtil.checkIfDateExist(currentDate.plusDays(1)));
+        assertTrue(DateUtil.checkIfDateExist(currentDate.plusDays(365))); 
+               
+        //day and month are diff
+        currentDate = currentDate.plusDays(1);
+        currentDate = currentDate.plusMonths(1);
+        assertTrue(DateUtil.checkIfDateExist(currentDate));
+        
+        //day and year diff
+        currentDate = currentDate.minusMonths(1);
+        currentDate = currentDate.plusYears(1);
+        assertTrue(DateUtil.checkIfDateExist(currentDate));
+        
+        //month and year diff
+        currentDate = currentDate.minusDays(1);
+        currentDate = currentDate.plusMonths(1);
+        assertTrue(DateUtil.checkIfDateExist(currentDate));
+    }
+    
+    //@@author A0139922Y
+    @Test
+    public void testCheckIfTimeExist_false() {
+        LocalDateTime currentTime = LocalDateTime.now();
+        assertNotNull(DateUtil.checkIfTimeExist(currentTime));
+        assertFalse(DateUtil.checkIfTimeExist(currentTime));
+    }
+    
+    //@@author A0139922Y
+    @Test
+    public void testIfTimeExist_true() {
+        LocalDateTime currentTime = LocalDateTime.now().toLocalDate().atTime(10,0);
+        assertNotNull(DateUtil.checkIfTimeExist(currentTime));
+        assertTrue(DateUtil.checkIfTimeExist(currentTime.toLocalDate().atTime(currentTime.getHour() - currentTime.getHour(), 
+                currentTime.getMinute())));
+        assertTrue(DateUtil.checkIfTimeExist(currentTime.toLocalDate().atTime(currentTime.getHour(), 
+                currentTime.getMinute() - currentTime.getMinute())));
+        assertTrue(DateUtil.checkIfTimeExist(currentTime.toLocalDate().atTime(currentTime.getHour() - currentTime.getHour(), 
+                currentTime.getMinute() - currentTime.getMinute())));
+    }
+    
+    //@@author A0139922Y
+    @Test
+    public void testParseTimeStamp_task_setTimeToMin() {
+        //set the time to 00:00 for task
+        assertNotEquals(DateUtil.parseTimeStamp(currentDay, null, true), currentDay);
+        assertEquals(DateUtil.parseTimeStamp(currentDay, null, true), DateUtil.floorDate(currentDay));
+    }
+    
+    //@@author A0139922Y
+    @Test
+    public void testParseTimeStamp_event_setTimeToMin() {
+        //will set the time to 00:00 for event
+        assertNotEquals(DateUtil.parseTimeStamp(currentDay, nextDay, true), currentDay);
+        assertEquals(DateUtil.parseTimeStamp(currentDay, nextDay, true), DateUtil.floorDate(currentDay));
+    }
+    
+    //@@author A0139922Y
+    @Test
+    public void testParseTimeStamp_event_setTimeToMax() {
+        // will set the time to 23:59
+        assertNotEquals(DateUtil.parseTimeStamp(nextDay, currentDay, false), nextDay);
+        assertEquals(DateUtil.parseTimeStamp(nextDay, currentDay, false), nextDayAt2359);
+    }
+
+    //@@author A0139922Y
+    @Test
+    public void testParseTimeStamp_event_followNextDayTime() {
+        // will set the time to follow the nextDay
+        assertNotEquals(DateUtil.parseTimeStamp(currentDay, nextDayAt2Pm, true), currentDay);
+        assertEquals(DateUtil.parseTimeStamp(currentDay, nextDayAt2Pm, true), currentDayAt2Pm);
+    }
+    
+    //@@author A0139922Y
+    @Test
+    public void testParseTimeStamp_event_followCurrentDayTime() {
+        // will set the time to follow the currentDay
+        assertEquals(DateUtil.parseTimeStamp(currentDayAt2Pm, nextDay, true), currentDayAt2Pm);
+        assertNotEquals(DateUtil.parseTimeStamp(nextDay, currentDayAt2Pm, false), nextDay);
+        assertEquals(DateUtil.parseTimeStamp(nextDay, currentDayAt2Pm, false), nextDayAt2Pm);
+    }
+
+    //@@author A0139922Y
+    @Test
+    public void testParseTimeStamp_event_followGivenDateTime() {
+        // if date and time exist, will not overwrite it
+        assertEquals(DateUtil.parseTimeStamp(currentDayAt2Pm, nextDay, true), currentDayAt2Pm);
+        assertEquals(DateUtil.parseTimeStamp(currentDayAt2Pm, nextDayAt2Pm, true), currentDayAt2Pm);
+        assertEquals(DateUtil.parseTimeStamp(nextDayAt2Pm, currentDay, false), nextDayAt2Pm);
+        assertEquals(DateUtil.parseTimeStamp(nextDayAt2Pm, currentDayAt2Pm, false), nextDayAt2Pm);
+    }
 }
