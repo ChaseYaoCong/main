934fc956f5ee41956946393fa34cbaf7efd6f26e
Chase <tiongyaocong@gmail.com>
2016-11-06 13:26:31 +0800

    forgot to push up, fix clear controller logic error

--- src/main/java/seedu/todo/controllers/ClearController.java
+++ src/main/java/seedu/todo/controllers/ClearController.java
@@ -264,7 +264,7 @@ public class ClearController implements Controller {
             // Filter by single date
             filteredTasks = FilterUtil.filterTaskBySingleDate(tasks, dateOn);
             filteredEvents = FilterUtil.filterEventBySingleDate(events, dateOn);
-        } else if (dateFrom != null && dateTo != null) {
+        } else if (dateFrom != null || dateTo != null) {
             // Filter by range
             filteredTasks = FilterUtil.filterTaskWithDateRange(tasks, dateFrom, dateTo);
             filteredEvents =FilterUtil.filterEventWithDateRange(events, dateFrom, dateTo);
