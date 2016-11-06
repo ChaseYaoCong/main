65794936a020af576d3c57edd59287ff425bbac8
Chase <tiongyaocong@gmail.com>
2016-11-06 13:00:39 +0800

    update list controller

--- src/main/java/seedu/todo/controllers/ListController.java
+++ src/main/java/seedu/todo/controllers/ListController.java
@@ -185,6 +185,9 @@ public class ListController implements Controller {
         
         if (dateCriteria != null) {
             // Filter by single date
+            assert dateOn == null;
+            assert dateFrom == null;
+            assert dateTo == null;
             filteredTasks = FilterUtil.filterTaskBySingleDate(tasks, dateCriteria);
             filteredEvents = FilterUtil.filterEventBySingleDate(events, dateCriteria);
         }
@@ -193,7 +196,7 @@ public class ListController implements Controller {
             // Filter by single date
             filteredTasks = FilterUtil.filterTaskBySingleDate(tasks, dateOn);
             filteredEvents = FilterUtil.filterEventBySingleDate(events, dateOn);
-        } else if (dateFrom != null && dateTo != null){
+        } else if (dateFrom != null && dateTo != null) {
             // Filter by range
             filteredTasks = FilterUtil.filterTaskWithDateRange(tasks, dateFrom, dateTo);
             filteredEvents =FilterUtil.filterEventWithDateRange(events, dateFrom, dateTo);
