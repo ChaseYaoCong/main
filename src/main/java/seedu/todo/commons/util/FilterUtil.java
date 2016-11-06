c51c856f93183f77317e623c3230951ee32d5684
Chase <tiongyaocong@gmail.com>
2016-11-06 17:05:39 +0800

    Finish commenting on filter util

--- src/main/java/seedu/todo/commons/util/FilterUtil.java
+++ src/main/java/seedu/todo/commons/util/FilterUtil.java
@@ -244,24 +244,30 @@ public class FilterUtil {
      *             Provided list for filtering
      * @param namelist
      *             Search and filter based on the name list
+     * @return filteredEvents
+     *              List containing only Event that event name matches with the item name
      */
     public static List<Event> filterEventByNames(List<Event> events, HashSet<String> nameList) {
         List<Event> filteredEvents = new ArrayList<Event>();
-        //if name list size is 0, means not searching by tags 
+        // If name list size is 0, means not searching by name list 
         if (nameList.size() == 0) {
             return filteredEvents;
         }
         
+        // Lopp through all the events
         for (int i = 0; i < events.size(); i ++) {
             Event event = events.get(i);
+            // Loop through all the name list
             Iterator<String> nameListIterator = nameList.iterator();
             while (nameListIterator.hasNext()) {
                 String matchingName = nameListIterator.next().toLowerCase();
+                // If found a match with its full name or sub names, break 
                 if (matchWithFullName(event, matchingName) || matchWithSubName(event, matchingName)) {
                     filteredEvents.add(event);
                     break;
                 }
             }
+            // Reset the nameList for the next event
             nameListIterator = nameList.iterator();
         }
         return filteredEvents;
@@ -273,25 +279,32 @@ public class FilterUtil {
      *             Provided list for filtering
      * @param namelist
      *             Search and filter based on the name list
+     * @return filteredEvents
+     *              List containing only Event that tag names matches with the tag name
      */
     public static List<Event> filterEventByTags(List<Event> events, HashSet<String> nameList) {
         List<Event> filteredEvents = new ArrayList<Event>();
-        //if name list size is 0, means not searching by tags 
+        // If name list size is 0, means not searching by tags 
         if (nameList.size() == 0) {
             return filteredEvents;
         }
         
+        // Loop through all the events
         for (int i = 0; i < events.size(); i ++) {
             Event event = events.get(i);
+            // Get the tag list of the event
             ArrayList<String> taskTagList = event.getTagList();
             Iterator<String> nameListIterator = nameList.iterator();
+            // Loop through the tag names list
             while (nameListIterator.hasNext()) {
                 String currentMatchingName = nameListIterator.next();
+                // If found a match, add and break
                 if (taskTagList.contains(currentMatchingName)) {
                     filteredEvents.add(event);
                     break;
                 }
             }
+            // Reset the tag names list for the next event
             nameListIterator = nameList.iterator();
         }
         return filteredEvents;
@@ -302,16 +315,21 @@ public class FilterUtil {
      * @param events 
      *             Provided list for filtering
      * @param isEventOver
-     *             True if searching for event that are over, false if searching for current event            
+     *             True if searching for event that are over, false if searching for current event  
+     * @return filteredEvents
+     *              List containing only Event that status matches with the status, For e.g. over or current
      */            
     public static List<Event> filterEventsByStatus(List<Event> events, boolean eventStatus) {
+        // If events is empty, return immediately
         if (events.size() == 0) {
             return events;
         }
         
         List<Event> filteredEvents = new ArrayList<Event>();
+        // Loop through the events
         for (int i = 0; i < events.size(); i ++) {
             Event event = events.get(i);
+            // Add into filteredEvents, once the status of the event matches
             if (event.isOver() == eventStatus) {
                 filteredEvents.add(event);
             }
@@ -324,20 +342,31 @@ public class FilterUtil {
      * @param events 
      *             Provided list for filtering
      * @param date            
-     *             Search based on this date         
+     *             Search based on this date
+     * @return filteredEvents
+     *              List containing only Event date that matches with searched date
      */
     public static List<Event> filterEventBySingleDate(List<Event> events, LocalDateTime date) {
+        // If events is empty, return immediately
         if (events.size() == 0) {
             return events;
         }
         
         ArrayList<Event> filteredEvents = new ArrayList<Event>();
+        // Loop through all the events
         Iterator<Event> iterator = events.iterator();
         while (iterator.hasNext()) {
             Event event = iterator.next();
+            // Search dates cannot be null
+            assert date != null;
             date = DateUtil.floorDate(date);
+            
+            // Event start date should not be null
+            assert event.getStartDate() != null; 
             LocalDateTime eventDate = DateUtil.floorDate(event.getStartDate());
-            if (eventDate != null && eventDate.equals(date)) {
+            
+            // Check against start date, if equals add it in
+            if (eventDate.equals(date)) {
                 filteredEvents.add(event);
             }
         }
@@ -352,34 +381,37 @@ public class FilterUtil {
      *             Search based on this as starting date
      * @param endDate            
      *             Search based on this as ending date
+     * @return filteredEvents
+     *              List containing only Event that date fall between startDate and endDate
      */
     public static List<Event> filterEventWithDateRange(List<Event> events, LocalDateTime startDate, LocalDateTime endDate) {
+        // If events is empty, return immediately
         if (events.size() == 0) {
             return events;
         }
     
+        // Set startDate to MIN, user could search "from Today"
         if (startDate == null) {
             startDate = LocalDateTime.MIN;
         }
         
+        // Set endDate to MaX, user could search "to Today"
         if (endDate == null) {
             endDate = LocalDateTime.MAX;
         }
         
         ArrayList<Event> filteredEvents = new ArrayList<Event>();
         Iterator<Event> iterator = events.iterator();
+        // Loop through all the events
         while (iterator.hasNext()) {
             Event event = iterator.next();
+            // Event dates should not be null
+            assert event.getStartDate() != null;
+            assert event.getEndDate() != null;
             LocalDateTime eventStartDate = DateUtil.floorDate(event.getStartDate());
             LocalDateTime eventEndDate = DateUtil.floorDate(event.getEndDate());
             
-            if (eventStartDate == null) {
-                eventStartDate = DateUtil.floorDate(LocalDateTime.MIN);
-            }
-            if (eventEndDate == null) {
-                eventEndDate = DateUtil.floorDate(LocalDateTime.MAX);
-            }
-            
+            // Check if the event start dates and end dates is within the search dates
             startDate = DateUtil.floorDate(startDate);
             endDate = DateUtil.ceilDate(endDate);
             if (eventStartDate.compareTo(startDate) >= 0 && eventEndDate.compareTo(endDate) <= 0) {
